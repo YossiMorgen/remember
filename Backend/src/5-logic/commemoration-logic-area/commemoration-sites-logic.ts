56c2dd51eb25cb31f3560b346638f357b1ee7172
yossi <a534158711@gmail.com>
2023-05-16 14:11:08 +0300

    CommemorationSiteLogic

---
+++ Backend/src/5-logic/commemoration-logic-area/commemoration-sites-logic.ts
@@ -0,0 +1,82 @@
+import { OkPacket } from "mysql";
+import dal from "../../2-utils/dal";
+import fileHandler from "../../2-utils/file-handler";
+import CommemorationSitesModel from "../../4-models/commemorations-models/commemoration-site-model";
+import { ValidationErrorModel } from "../../4-models/error-models";
+import e from "express";
+
+async function getAllCommemorationSitesByCommemorative(commemorativeID : number) {
+    const sql = `SELECT * FROM commemorationSites WHERE commemorativeID = ?`;
+    const commemorationSites = await dal.execute(sql, [commemorativeID]);
+    return commemorationSites;
+}
+
+async function addCommemorationSite(commemorationSite: CommemorationSitesModel) {
+    const err = commemorationSite.validation();
+    if (err) throw new ValidationErrorModel(err);
+
+    commemorationSite.imageName = await fileHandler.saveFile(commemorationSite.image);
+    delete commemorationSite.image;
+
+    const sql = `INSERT INTO commemorationSites VALUES (DEFAULT, ?, ?, ?, ?, ?, ?)`;
+    const info:OkPacket = await dal.execute(sql, [
+        commemorationSite.commemorativeID,
+        commemorationSite.commemorationName,
+        commemorationSite.commemorationAddress,
+        commemorationSite.imageName,
+        commemorationSite.description,
+        commemorationSite.connection,
+    ]);
+
+    commemorationSite.commemorationSiteID = info.insertId;
+    return commemorationSite;
+}
+
+async function updateCommemorationSite(commemorationSite: CommemorationSitesModel) {
+    const err = commemorationSite.validation();
+    if (err) throw new ValidationErrorModel(err);
+
+    if (commemorationSite.image){
+        const oldCommemorationSite = await getCommemorationSiteByID(commemorationSite.commemorationSiteID);
+        fileHandler.deleteFile(oldCommemorationSite.imageName);
+
+        commemorationSite.imageName = await fileHandler.saveFile(commemorationSite.image);
+        delete commemorationSite.image;
+    }
+
+
+    const sql = `UPDATE commemorationSites SET commemorativeID = ?, commemorationName = ?, commemorationAddress = ?, imageName = ?, description = ?, connection = ? WHERE commemorationSiteID = ?`;
+    await dal.execute(sql, [
+        commemorationSite.commemorativeID,
+        commemorationSite.commemorationName,
+        commemorationSite.commemorationAddress,
+        commemorationSite.imageName,
+        commemorationSite.description,
+        commemorationSite.connection,
+        commemorationSite.commemorationSiteID,
+    ]);
+
+    return commemorationSite;
+}
+
+async function deleteCommemorationSite(commemorationSiteID: number) {
+    const commemorationSite = await getCommemorationSiteByID(commemorationSiteID);
+    fileHandler.deleteFile(commemorationSite.imageName);
+
+    const sql = `DELETE FROM commemorationSites WHERE commemorationSiteID = ?`;
+    await dal.execute(sql, [commemorationSiteID]);
+}
+
+async function getCommemorationSiteByID(commemorationSiteID: number) {
+    const sql = `SELECT * FROM commemorationSites WHERE commemorationSiteID = ?`;
+    const [commemorationSite] = await dal.execute(sql, [commemorationSiteID]);
+    return commemorationSite;
+}
+
+export default {
+    getAllCommemorationSitesByCommemorative,
+    addCommemorationSite,
+    updateCommemorationSite,
+    deleteCommemorationSite,
+    getCommemorationSiteByID,
+}
