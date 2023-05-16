8b8ea5158fbd66e1f69692db1bd65f99680c60d6
yossi <a534158711@gmail.com>
2023-05-16 07:25:54 +0300

    CommemorativeModel validationSchema

--- Backend/src/4-models/commemorations-models/commemoration-model.ts
+++ Backend/src/4-models/commemorations-models/commemoration-model.ts
@@ -1,4 +1,5 @@
 import { UploadedFile } from "express-fileupload";
+import Joi from "joi";
 
 export default class CommemorativeModel {
     public commemorativeID : number;
@@ -9,7 +10,7 @@ export default class CommemorativeModel {
     public deceaseImageName: string;
     public language: string;
     public birthDate: Date;
-    public deathDay: Date;
+    public deathDate: Date;
     public state: string;
     public partnerType: string;
     public partnerName: string;
@@ -31,7 +32,7 @@ export default class CommemorativeModel {
         this.deceaseImageName = commemorative.deceaseImageName;
         this.language = commemorative.language;
         this.birthDate = commemorative.birthDate;
-        this.deathDay = commemorative.deathDay;
+        this.deathDate = commemorative.deathDate;
         this.state = commemorative.state;
         this.partnerType = commemorative.partnerType;
         this.partnerName = commemorative.partnerName;
@@ -46,5 +47,31 @@ export default class CommemorativeModel {
     }
 
 
+    public static validationSchema = Joi.object({
+        commemorativeID: Joi.string().optional(),
+        deceasedName: Joi.string().min(2).max(30).required(),
+        biography: Joi.string().min(2).max(200).required(),
+        about: Joi.string().min(2).max(200).required(),
+        deceaseImage: Joi.object().optional(),
+        deceaseImageName: Joi.date().optional(),
+        language: Joi.string().valid('english', 'hebrew'),
+        birthDate: Joi.date().required(),
+        deathDate: Joi.date().required(),
+        state: Joi.string().min(2).max(15).required(),
+        partnerType: Joi.string().valid('wife', 'husband').required(),
+        partnerName: Joi.string().min(2).max(20).required(),
+        fatherName: Joi.string().min(2).max(20),
+        motherName: Joi.string().min(2).max(20),
+        childrenNames: Joi.string().min(2).max(400).required(),
+        graveImage: Joi.object().optional(),
+        graveImageName: Joi.string().max(150).optional(),
+        graveYardName: Joi.string().min(2).max(20).required(),
+        locationLink: Joi.string().min(2).max(100).optional(),
+        views: Joi.number().optional()
+    })
 
+    public validation():string{
+        const res = CommemorativeModel.validationSchema.validate(this);
+        return res.error?.message;
+    }
 }
