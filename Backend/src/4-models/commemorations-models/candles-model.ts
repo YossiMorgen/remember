caa0dc42c692d3934582fa32518c7dfc6fe227be
yossi <a534158711@gmail.com>
2023-05-16 08:10:01 +0300

    CandlesModel

---
+++ Backend/src/4-models/commemorations-models/candles-model.ts
@@ -0,0 +1,27 @@
+import Joi from "joi";
+
+export default class CandlesModel {
+    public commemorativeID: number;
+    public userID: number;
+    public amount: number;
+    public LastUpdate: Date;
+
+    public constructor(candle: CandlesModel){
+        this.commemorativeID = candle.commemorativeID;
+        this.userID = candle.userID;
+        this.amount = candle.amount;
+        this.LastUpdate = candle.LastUpdate;
+    }
+
+    public static validationSchema = Joi.object({
+        commemorativeID: Joi.number().optional().integer().positive(),
+        userID : Joi.number().optional().integer().positive(),
+        amount: Joi.number().optional().integer().positive(),
+        LastUpdate: Joi.date().optional(),
+    })
+
+    public validation():string{
+        const res = CandlesModel.validationSchema.validate(this);
+        return res.error?.message;
+    }
+}
