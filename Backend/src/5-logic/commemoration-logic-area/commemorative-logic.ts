069f4d57c77eac035dac719d8fe75affef385959
yossi <a534158711@gmail.com>
2023-05-16 13:59:11 +0300

    commemorationSites Logic

--- Backend/src/5-logic/commemoration-logic-area/commemorative-logic.ts
+++ Backend/src/5-logic/commemoration-logic-area/commemorative-logic.ts
@@ -4,18 +4,20 @@ import fileHandler from "../../2-utils/file-handler";
 import CommemorativeModel from "../../4-models/commemorations-models/commemorative-model";
 import { ValidationErrorModel } from "../../4-models/error-models";
 import appConfig from "../../2-utils/AppConfig";
+import LanguageModel from "../../4-models/languages-model";
 
-async function getRandomCommemorative(offset: number){
+async function getRandomCommemorative(offset: number, language: LanguageModel){
     const limit = 10;
 
     const sql = `
         SELECT commemorativeID, deceasedName, deceaseImageName, deathDate 
         FROM commemorative
+        WHERE language = ?
         LIMIT ?
         OFFSET ?
     `
 
-    const commemorative = await dal.execute(sql, [limit, offset])
+    const commemorative = await dal.execute(sql, [language, limit, offset])
 
     return commemorative;
 
@@ -69,3 +71,23 @@ async function addCommemorative (commemorative: CommemorativeModel){
     return commemorative;
 }
 
+async function updateCommemorative (commemorative: CommemorativeModel){
+}
+
+async function deleteCommemorative (commemorativeID: number){
+    const oldCommemorative = await getCommemorativeByID(commemorativeID);
+
+    fileHandler.deleteFile(oldCommemorative.deceaseImageName);
+    fileHandler.deleteFile(oldCommemorative.graveImageName);
+
+    const sql = `DELETE FROM commemorative WHERE commemorativeID = ?`
+    await dal.execute(sql, [commemorativeID]);
+}
+
+export default {
+    getRandomCommemorative,
+    getCommemorativeByID,
+    addCommemorative,
+    updateCommemorative,
+    deleteCommemorative
+}
