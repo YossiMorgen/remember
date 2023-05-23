0199a99dfa219e77488c7a36ef3bfab7bdbba285
yossi <a534158711@gmail.com>
2023-05-23 15:35:13 +0300

    add_commemorative not available when not logged in

--- Frontend/src/app/components/layout-area/header/header.component.ts
+++ Frontend/src/app/components/layout-area/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit, LOCALE_ID, Inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { ConfigService } from 'src/app/utils/config.service';
 
@@ -10,19 +10,18 @@ import { ConfigService } from 'src/app/utils/config.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  public path: string = this.location.path().split('/')[1];
+  public url: string = this.router.url.split('/')[1];
   public language = 'en'
   constructor(
     public authService: AuthService,
     @Inject(LOCALE_ID) public locale: string,
-    public location: Location,
-    private router: Router,
+    public router: Router,
     public config: ConfigService
   ) { }
 
   ngOnInit(): void {
     this.router.events.subscribe((val) => {
-      this.path = this.location.path().split('/')[1];      
+      this.url = this.router.url.split('/')[1]; 
     });
 
     this.language = this.locale;    
